79a0450adc192469247cfd28449c4ac008ac9699
its-daaghi <1muhammadahmad0313@gmail.com>
2026-02-10 10:20:30 +0500

    Add Basic Urls

--- BookReview/book/urls.py
+++ BookReview/book/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("book/new", views.Book_create, name="Book_create"),
-    path("", views.booklist, name="booklist"),
-    path("book/<int:pk>/", views.bookdetail, name="bookdetail"),
-    path("book/<int:pk>/edit/", views.book_update, name="bookupdate"),
-    path("book/<int:pk>/delete/", views.book_delete, name="bookdelete"),
+    path("book/new", views.BookCreateView.as_view(), name="Book_create"),
+    path("", views.BookListView.as_view(), name="booklist"),
+    path("book/<int:pk>/", views.BookDetailView(), name="bookdetail"),
+    path("book/<int:pk>/edit/", views.BookUpdateView.as_view(), name="bookupdate"),
+    path("book/<int:pk>/delete/", views.BookDeleteView.as_view(), name="bookdelete"),
 ]
